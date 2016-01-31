f04eab552e1c5b9682c9b1f18814e682bcecd6bf
laf <neil@lathwood.co.uk>
2016-01-31 18:21:54 +0000

    Updated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,9 +34,3 @@ Route::group(['middleware' => 'web'], function () {
     });
 
 });
-
-Route::group(['middleware' => 'web'], function () {
-    Route::auth();
-
-    Route::get('/home', 'HomeController@index');
-});
